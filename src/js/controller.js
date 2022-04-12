91ad1ac3e45b251d74e044f0d54c8f259c4ff3e1
Moustaph Diakite <63529813+moustaphdiakite@users.noreply.github.com>
2022-04-12 19:52:57 -0300

    Add icons to the page

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,5 @@
+import icons from 'url:../img/icons.svg';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -44,7 +46,7 @@ async function showRecipe() {
         <div class="recipe__details">
           <div class="recipe__info">
             <svg class="recipe__info-icon">
-              <use href="src/img/icons.svg#icon-clock"></use>
+              <use href="src/img/icons.sv${icons}#icon-clock"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--minutes">${
               recipe.cookingTime
@@ -53,7 +55,7 @@ async function showRecipe() {
           </div>
           <div class="recipe__info">
             <svg class="recipe__info-icon">
-              <use href="src/img/icons.svg#icon-users"></use>
+              <use href="${icons}#icon-users"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--people">${
               recipe.servings
@@ -63,12 +65,12 @@ async function showRecipe() {
             <div class="recipe__info-buttons">
               <button class="btn--tiny btn--increase-servings">
                 <svg>
-                  <use href="src/img/icons.svg#icon-minus-circle"></use>
+                  <use href="${icons}#icon-minus-circle"></use>
                 </svg>
               </button>
               <button class="btn--tiny btn--increase-servings">
                 <svg>
-                  <use href="src/img/icons.svg#icon-plus-circle"></use>
+                  <use href="${icons}#icon-plus-circle"></use>
                 </svg>
               </button>
             </div>
@@ -76,12 +78,12 @@ async function showRecipe() {
 
           <div class="recipe__user-generated">
             <svg>
-              <use href="src/img/icons.svg#icon-user"></use>
+              <use href="${icons}#icon-user"></use>
             </svg>
           </div>
           <button class="btn--round">
             <svg class="">
-              <use href="src/img/icons.svg#icon-bookmark-fill"></use>
+              <use href="${icons}#icon-bookmark-fill"></use>
             </svg>
           </button>
         </div>
@@ -94,7 +96,7 @@ async function showRecipe() {
               return `
               <li class="recipe__ingredient">
                 <svg class="recipe__icon">
-                  <use href="src/img/icons.svg#icon-check"></use>
+                  <use href="${icons}#icon-check"></use>
                 </svg>
                 <div class="recipe__quantity">${ing.quantity}</div>
                 <div class="recipe__description">
@@ -128,7 +130,7 @@ async function showRecipe() {
           >
             <span>Directions</span>
             <svg class="search__icon">
-              <use href="src/img/icons.svg#icon-arrow-right"></use>
+              <use href="${icons}#icon-arrow-right"></use>
             </svg>
           </a>
         </div>`;
