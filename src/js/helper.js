7f04c5ba499adcf89001b549546e377e2cf11107
Moustaph Diakite <63529813+moustaphdiakite@users.noreply.github.com>
2022-04-13 14:37:12 -0300

    Create helper.js

---
+++ src/js/helper.js
@@ -0,0 +1,21 @@
+import { async } from 'regenerator-runtime';
+import { API_URL, TIMEOUT_SEC } from './config';
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+export async function getJSON(url) {
+  try {
+    const response = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
+    const data = await response.json();
+    if (!response.ok) throw new Error(`${data.message} (${response.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+}
