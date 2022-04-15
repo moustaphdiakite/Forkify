610d8a08f98f8e702fe090a529a9a43549376165
Moustaph Diakite <63529813+moustaphdiakite@users.noreply.github.com>
2022-04-15 11:03:41 -0300

    Replace private field notation

--- src/js/Views/searchView.js
+++ src/js/Views/searchView.js
@@ -2,20 +2,20 @@ import icons from 'url:../../img/icons.svg';
 import { Fraction } from 'fractional';
 
 class SearchView {
-  #parentEl = document.querySelector('.search');
+  _parentEl = document.querySelector('.search');
 
   getQuery() {
-    const query = this.#parentEl.querySelector('.search__field').value;
-    this.#clearInput();
+    const query = this._parentEl.querySelector('.search__field').value;
+    this._clearInput();
     return query;
   }
 
-  #clearInput() {
-    this.#parentEl.querySelector('.search__field').value = '';
+  _clearInput() {
+    this._parentEl.querySelector('.search__field').value = '';
   }
 
   addHandlerSearch(handler) {
-    this.#parentEl.addEventListener('submit', function (e) {
+    this._parentEl.addEventListener('submit', function (e) {
       e.preventDefault();
       handler();
     });
